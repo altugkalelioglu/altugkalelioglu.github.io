311a889bc67168d06f9a20d0fba01c52043bbfa9
altugkalelioglu <altugkalelioglu@gmail.com>
2026-03-21 00:53:24 +0300

    Create sw.js

---
+++ life/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'life-productivity-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(req)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            if (req.url.startsWith(self.location.origin)) {
+              cache.put(req, copy);
+            }
+          });
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    })
+  );
+});
